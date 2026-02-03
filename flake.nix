a522f594fc9ae3c7ef929574900cc6e442e02346
momo <luna.cloudberry@gmail.com>
2026-02-03 23:45:31 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          pkgs.mkShell rec {
+            nativeBuildInputs = [
+              nickel
+            ];
+            buildInputs = [
+              pkg-config
+              zlib
+            ];
+
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+
+          };
+      }
+    );
+}
